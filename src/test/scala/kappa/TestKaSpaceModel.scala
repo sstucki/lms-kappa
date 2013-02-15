5eb775ed908e21a383b1f2b79c33b25bad12213c
Sandro Stucki <sandro.stucki@gmail.com>
2013-02-15 01:37:26 +0100

    Fixes bugs and adds observables in TestKaSpaceModel.

--- src/test/scala/kappa/TestKaSpaceModel.scala
+++ src/test/scala/kappa/TestKaSpaceModel.scala
@@ -31,16 +31,31 @@ class TestKaSpaceModel extends KaSpaceModel with FlatSpec
 
   // Rules
   val bindLR = "A(r) , A(l) " -> s"A(r!1) , A(l!1),  1:$wLR.$wRL" :@ 1
-  val bindBL = "A(bl), A(bl)" -> s"A(bl!1), A(bl!1), 2:$wBL.$wBL" :@ 1
-  val bindBR = "A(br), A(br)" -> s"A(br!1), A(br!1), 3:$wBR.$wBR" :@ 1
+  val bindBL = "A(bl), A(bl)" -> s"A(bl!1), A(bl!1), 1:$wBL.$wBL" :@ 1
+  val bindBR = "A(br), A(br)" -> s"A(br!1), A(br!1), 1:$wBR.$wBR" :@ 1
   val unbindLR = "A(r!1) , A(l!1) " -> "A(r) , A(l) " :@ 1
   val unbindBL = "A(bl!1), A(bl!1)" -> "A(bl), A(bl)" :@ 1
   val unbindBR = "A(br!1), A(br!1)" -> "A(br), A(br)" :@ 1
 
-  val m: Mixture = p"A:$radius(l:$posL, r:$posR, bl:$posBL, br:$posBR)"
-  withInit(m * 50)
-  withMaxTime(10.0)
+  withInit(s"A:$radius(l:$posL, r:$posR, bl:$posBL, br:$posBR)", 50)
 
+  //print(mix)
+
+  // Expected observables
+  withObs("A(r!1, br!2), A(l!1, bl!3), A(br!2, bl!3)", "A3t")
+  withObs("A(r!1), A(l!1)", "A.r!l.A")
+  withObs("A(l!1), A(r!1, l!2), A(r!2, l!3), A(r!3, l!4), A(r!4, l!5)," +
+    "A(r!5, l!6), A(r!6)", "A7c")
+  withObs("A(r!1, l!2), A(r!2, l!3), A(r!3, l!4), A(r!4, l!5), A(r!5, l!6)," +
+    "A(r!6, l!7), A(r!7, l!1)", "A7r")
+
+  // Unexpected observables
+  withObs("A(r!1, l!2), A(r!2, l!3), A(r!3, l!1)", "A3r")
+  withObs("A(r!1, bl!2), A(l!1, br!3), A(bl!2, br!3)", "A3t'")
+  withObs("A(l!1), A(r!1, l!2), A(r!2, l!3), A(r!3, l!4), A(r!4, l!5)," +
+    "A(r!5, l!6), A(r!6, l!7), A(r!7)", "A8c")
+
+  withMaxTime(10.0)
   run
 }
 
