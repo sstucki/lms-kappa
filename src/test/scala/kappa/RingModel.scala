9d55c91b8870ca523ba33c33a1f488504904141c
Sandro Stucki <sandro.stucki@gmail.com>
2013-02-16 21:55:58 +0100

    Updates models.

--- src/test/scala/kappa/RingModel.scala
+++ src/test/scala/kappa/RingModel.scala
@@ -16,28 +16,51 @@ class RingModel extends KaSpaceModel with FlatSpec
   val wLR = Orientation.y(-Pi * 2 / 3)
   val wRL = Orientation.y( Pi * 2 / 3)
 
-  contactGraph = s"""A:$radius (b:{$posL}!{1}, c:{$posR}!{2}),
-                     B:$radius (a:{$posR}!{1}, c:{$posL}!{3}),
-                     C:$radius (a:{$posL}!{2}, b:{$posR}!{3}),
+  contactGraph = s"""A:{$radius} (b:{$posL}!{1}, c:{$posR}!{2}),
+                     B:{$radius} (a:{$posR}!{1}, c:{$posL}!{3}),
+                     C:{$radius} (a:{$posL}!{2}, b:{$posR}!{3}),
                      1:{$wLR.$wRL},
                      2:{$wRL.$wLR},
                      3:{$wLR.$wRL}"""
 
+  // Rate constants
+  val on_rate = 1
+  val off_rate = 1
+  val close_rate = 10
+
   // Rules
-  val bindAB = "A(b), B(a)" -> "A(b!1), B(a!1), 1:" :@ 1
-  val bindAC = "A(c), C(a)" -> "A(c!1), C(a!1), 1:" :@ 1
-  val bindBC = "B(c), C(b)" -> "B(c!1), C(b!1), 1:" :@ 1
-  val unbindAB = "A(b!1), B(a!1)" -> "A(b), B(a)" :@ 1
-  val unbindAC = "A(c!1), C(a!1)" -> "A(c), C(a)" :@ 1
-  val unbindBC = "B(c!1), C(b!1)" -> "B(c), C(b)" :@ 1
+  val bindAB = "A(b), B(a)" -> s"A(b!1), B(a!1), 1:$wLR.$wRL" :@ on_rate
+  val bindAC = "A(c), C(a)" -> s"A(c!1), C(a!1), 1:$wRL.$wLR" :@ on_rate
+  val bindBC = "B(c), C(b)" -> s"B(c!1), C(b!1), 1:$wLR.$wRL" :@ on_rate
+  val unbindAB = "A(b!1), B(a!1)" -> "A(b), B(a)" :@ off_rate
+  val unbindAC = "A(c!1), C(a!1)" -> "A(c), C(a)" :@ off_rate
+  val unbindBC = "B(c!1), C(b!1)" -> "B(c), C(b)" :@ off_rate
+  val closeAB = "A(b, c!1), B(a, c!2), C(a!1, b!2)" ->
+    s"A(b!3, c!1), B(a!3, c!2), C(a!1, b!2), 1:$wLR.$wRL" :@ close_rate
+  val closeAC = "A(c, b!1), B(a!1, c!2), C(a, b!2)" ->
+    s"A(c!3, b!1), B(a!1, c!2), C(a!3, b!2), 1:$wRL.$wLR" :@ close_rate
+  val closeBC = "A(b!1, c!2), B(a!1, c), C(a!2, b)" ->
+    s"A(b!1, c!2), B(a!1, c!3), C(a!2, b!3), 1:$wLR.$wRL" :@ close_rate
+
+  // Expected observables
+  withObs("A(c!3, b!1), B(a!1, c!2), C(b!2, a!3)", "triangle")
+  withObs("A(b!1), B(a!1)", "A-B")
+  withObs("A(c!1), C(a!1)", "A-C")
+  withObs("B(c!1), C(b!1)", "B-C")
+  withObs("A(b!1), B(a!1, c!2), C(b!2)", "A-B-C")
+
+  // Unexpected observables
+  withObs("A(b!1), B(a!1, c!2), C(b!2, a!3), A(c!3)", "A-B-C-A")
+  withObs("A(c!6, b!1), B(a!1, c!2), C(b!2, a!3), A(c!3, b!4), "
+    + "B(a!4, c!5), C(b!5, a!6)", "hexagon")
 
   // Mixture
-  withInit(m"A:$radius (b:$posL, c:$posR)" * 50)
-  withInit(m"B:$radius (a:$posR, c:$posL)" * 50)
-  withInit(m"C:$radius (a:$posL, b:$posR)" * 50)
+  withInit(m"A:$radius (b:$posL, c:$posR)" * 200)
+  withInit(m"B:$radius (a:$posR, c:$posL)" * 200)
+  withInit(m"C:$radius (a:$posL, b:$posR)" * 200)
 
   // Simulate!
-  withMaxEvents(100)
+  withMaxEvents(10000)
   run
 }
 
--- src/test/scala/kappa/TestKaSpaceModel.scala
+++ src/test/scala/kappa/TestKaSpaceModel.scala
@@ -6,7 +6,7 @@ import scala.math._
 class ProteasomeModel extends KaSpaceModel with FlatSpec
 {
   // Agent radius
-  val radius: Double = 1
+  val radius: Double = 0.8
 
   // Site positions
   val posL  = Position(-cos(Pi/7), sin(Pi/7), 0)
@@ -31,8 +31,8 @@ class ProteasomeModel extends KaSpaceModel with FlatSpec
 
   // Rules
   val bindLR = "A(r) , A(l) " -> s"A(r!1) , A(l!1),  1:$wRL.$wLR" :@ 1
-  val bindBL = "A(bl), A(bl)" -> s"A(bl!1), A(bl!1), 2:$wBL.$wBL" :@ 1
-  val bindBR = "A(br), A(br)" -> s"A(br!1), A(br!1), 3:$wBR.$wBR" :@ 1
+  val bindBL = "A(bl), A(bl)" -> s"A(bl!1), A(bl!1), 1:$wBL.$wBL" :@ 1
+  val bindBR = "A(br), A(br)" -> s"A(br!1), A(br!1), 1:$wBR.$wBR" :@ 1
   val unbindLR = "A(r!1) , A(l!1) " -> "A(r) , A(l) " :@ 1
   val unbindBL = "A(bl!1), A(bl!1)" -> "A(bl), A(bl)" :@ 1
   val unbindBR = "A(br!1), A(br!1)" -> "A(br), A(br)" :@ 1
@@ -43,6 +43,8 @@ class ProteasomeModel extends KaSpaceModel with FlatSpec
   // Expected observables
   withObs("A(r!1, br!2), A(l!1, bl!3), A(br!2, bl!3)", "side triangle")
   withObs("A(r!1), A(l!1)", "R-L")
+  withObs("A(bl!1), A(bl!1)", "BL-BL")
+  withObs("A(br!1), A(br!1)", "BR-BR")
   withObs("A(l!1), A(r!1, l!2), A(r!2, l!3), A(r!3, l!4), A(r!4, l!5)," +
     "A(r!5, l!6), A(r!6)", "7 chain")
   withObs("A(r!1, l!2), A(r!2, l!3), A(r!3, l!4), A(r!4, l!5), A(r!5, l!6)," +
@@ -50,12 +52,12 @@ class ProteasomeModel extends KaSpaceModel with FlatSpec
 
   // Unexpected observables
   withObs("A(r!1, l!2), A(r!2, l!3), A(r!3, l!1)", "3 ring")
-  withObs("A(r!1, bl!2), A(l!1, br!3), A(bl!2, br!3)", "wrong side triangle")
+  withObs("A(r!1, bl!2), A(l!1, br!3), A(bl!2, br!3)", "wrong chirality triangle")
   withObs("A(l!1), A(r!1, l!2), A(r!2, l!3), A(r!3, l!4), A(r!4, l!5)," +
     "A(r!5, l!6), A(r!6, l!7), A(r!7)", "8 chain")
 
   // Simulate!
-  withMaxTime(10.0)
+  withMaxTime(100.0)
   run
 }
 
